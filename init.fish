46e7fcc8183d8c025f84d69ea81d2c53d2341c5c
davidtheghost <74461728+davidtheghost@users.noreply.github.com>
2022-07-29 14:05:42 +0200

    Adds init.fish

---
+++ init.fish
@@ -0,0 +1,15 @@
+# Color's
+set -g theme_color_host white
+set -g theme_color_user white
+set -g theme_color_separator white
+set -g theme_color_time green
+
+# Display's
+set -g theme_display_time yes
+set -g theme_display_group no
+set -g theme_display_rw no
+set -g theme_display_batt yes
+set -g theme_display_time_format '+%R'
+
+# No Vi Mode
+set -U fish_key_bindings fish_default_key_bindings

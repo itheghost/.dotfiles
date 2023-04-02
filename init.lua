3a2dde64922049214eef1e43bbbb8f6fb2234902
itheghost <74461728+itheghost@users.noreply.github.com>
2023-04-02 02:05:44 +0200

    Lsp config

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require('ghost.settings')
 require('ghost.keymaps')
-require('ghost.plugins')
+require('ghost.packer')
 --require('lualine').setup { options = { theme = 'nord' } }
 vim.cmd "colorscheme nord"
 vim.cmd "syntax on"
